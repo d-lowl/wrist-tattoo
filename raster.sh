362a9823d70b0e2abb294f1d2bfcdf8ea2a2a3f4
Twitter-kun <twitter@sihvi.com>
2017-05-24 00:22:12 +0100

    html example and color variants

--- raster.sh
+++ raster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-svgexport tattoo.svg tattoo.png 10x && convert tattoo.png -fuzz 15% -transparent white tattoo-alpha.png
+svgexport tattoo.svg tattoo.png 4x && convert tattoo.png -fuzz 15% -transparent white tattoo-alpha.png
